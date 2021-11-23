51df95cfa8e67d40d9077f69ff49083f12bb175a
Chris Peakin <christopher.peakin@googlemail.com>
2021-11-23 19:50:41 +0000

    solved all 6 tests, this time much more concisely

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 function calculateBill(price, vat, tip){
     const bill = price + (price*(vat/100)) + tip;
     const stringBill = bill.toString();
-    if ((typeof price) !== "number"){
-        return("error");
-    }
-    if ((typeof vat) !== "number"){
-        return("error");
-    }
-    if ((typeof tip) !== "number"){
-        return("error");
+    if ((typeof price) !== "number" || (typeof vat) !== "number" || (typeof tip) !== "number"){
+        return("error")
     }
     return('£'+stringBill);
 }
