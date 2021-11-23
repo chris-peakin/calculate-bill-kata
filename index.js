c15f694a99d1c0a693b03847ba6d907b32822440
Chris Peakin <christopher.peakin@googlemail.com>
2021-11-23 19:36:49 +0000

    solved all six tests, perhaps not the most eloquent way, am committing anyway to observe how or if it can be improved

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function calculateBill(){
-
+function calculateBill(price, vat, tip){
+    const bill = price + (price*(vat/100)) + tip;
+    const stringBill = bill.toString();
+    if ((typeof price) !== "number"){
+        return("error");
+    }
+    if ((typeof vat) !== "number"){
+        return("error");
+    }
+    if ((typeof tip) !== "number"){
+        return("error");
+    }
+    return('£'+stringBill);
 }
 
 module.exports = calculateBill;
